c6d8614d547a8a00f94d5a9ac03a2f448e17cec5
shiv farkale <shivfarkale66@gmail.com>
2026-03-12 15:22:41 +0530

    update

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ shiv() // call the shiv
     var a = 50 ;
 console.log(typeof (a));
 
+
+
+ function sid() {
+    var a =20 ;
+    var b = 10 ;
+    console.log (a +b) ;
+}
+sid() // call me sir 
+
+
