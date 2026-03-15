983fd13fdb27fa990d70ab8fde304ae4db1c963f
shiv farkale <shivfarkale66@gmail.com>
2026-03-15 20:09:59 +0530

    push

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const profile = {
-    name : " Shiv farkale" ,
-    follow : true ,
-    request  : 10 ,
-    link : "www.google.com" 
-
-
-}
-console.log(profile);
 
 
 
+
+// let age = 15
+
+// if (age >= 18) {
+//     console.log("vote me ")
+
+// } else {
+//     console.log("not vote");
+
+// }
+
+
+
+ let num = 15 ;
+ if (num % 2 === 0 ) { 
+    console.log("even")
+
+ }else {
+    console.log("odd")
+ }
 
