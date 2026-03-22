1c2a768694c7eb3609ad65e8a4dbeb6af84393a8
shiv farkale <shivfarkale66@gmail.com>
2026-03-23 01:00:38 +0530

    add file

--- DOM.js
+++ DOM.js
@@ -28,45 +28,110 @@
 // }
 
 
-function hideText() {
-    let inter = document.querySelector('.Text')
-    .style.display = "none";
-}
+// function hideText() {
+//     let inter = document.querySelector('.Text')
+//     .style.display = "none";
+// }
 
-function showText() {
-    document.querySelector('.Text')
+// function showText() {
+//     document.querySelector('.Text')
 
-    .style.display = "block" ;
-}
+//     .style.display = "block" ;
+// }
 
 
 
-let count = 0
+// let count = 0
 
- function increment() { 
-    count++
-     document.getElementById("count")
-     .innerHTML = count
-     }
+//  function increment() { 
+//     count++
+//      document.getElementById("count")
+//      .innerHTML = count
+//      }
  
 
  
-     function decrement(){ 
+//      function decrement(){ 
     
-      if ( count <=0 ){
-        return
+//       if ( count <=0 ){
+//         return
 
-      }    
-        count--    
+//       }    
+//         count--    
       
       
-       document.getElementById("count")
-    .innerHTML = count
+//        document.getElementById("count")
+//     .innerHTML = count
       
- }
+//  }
 
 
- function shiva() {
-     let  value =document.querySelector(".contener")
-    value.style.backgroundImage = "url(cat.webp)" 
+//  function shiva() {
+//      let  value =document.querySelector(".contener")
+//     value.style.backgroundImage = "url(cat.webp)" 
+// }
+
+function Red() {
+
+    document.querySelector('.Box').style.backgroundColor = "red " ;
+}
+
+function Green() {
+
+    document.querySelector('.Box').style.backgroundColor = "green " ;
+}
+function Blue(){
+
+    document.querySelector('.Box').style.backgroundColor = "blue " ;
+}
+
+// question two 
+
+let count = 0 ;
+function reset(){
+    count = 0 ;
+
+    document.querySelector('.Box').innerHTML= "count";
+}
+
+
+// amzon watch list creat
+
+function shirt() {
+    document.querySelector('.inerdiv')
+   .style.backgroundImage = "url(imgs/outfi.jpg)"
+
+}
+function menfation() {
+    document.querySelector('.inerdiv')
+   .style.backgroundImage = "url(imgs/menout.jpg)"
+
+}
+function shoes() {
+    document.querySelector('.inerdiv')
+   .style.backgroundImage = "url(imgs/sneakers.jpg)"
+
+}
+function watch() {
+    document.querySelector('.inerdiv')
+   .style.backgroundImage = "url(imgs/watch.jpg)"
+
+}
+
+
+// queston four
+
+function outputbox() {
+   let text = document.getElementById ("inputbox").value;
+   document.getElementById("output").innerText = text ;
+
+}
+
+// question five
+
+
+function changecolor() {
+    let color = document.getElementById("bginputbox").value ;
+    document.getElementById("changecolor")
+    .style.backgroundColor = color ;
 }
